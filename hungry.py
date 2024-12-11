f032001b8c0c5bc243c7f35841d4432ad00529a3
Upendra Awasthi <upendraawasthi88@gmail.com>
2024-12-11 15:24:33 +0100

    water

--- hungry.py
+++ hungry.py
@@ -1,7 +1,12 @@
 hungry = input('are you hungry?')
 if hungry=='yes':
     print("eat samosa")
+    print("eat pizza")
+    print("eat burger")
 else:
+    thirsty = input('are you thirsty?')
+    if thirsty=="yes":
+        print('drink water')
     print("do your homework")
 read = input('have read GOT?')
 if read=='yes':
