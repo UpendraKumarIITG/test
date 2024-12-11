a2541cf7956f3e0441cd5f2bcd67e8b4f4534d58
Upendra Awasthi <upendraawasthi88@gmail.com>
2024-12-11 15:06:29 +0100

    Revert "piza and burger"
    This reverts commit 5bd91e7fabf211e0e8049d5979a1b89fe6023976.

--- hungry.py
+++ hungry.py
@@ -1,8 +1,6 @@
 hungry = input('are you hungry?')
 if hungry=='yes':
     print("eat samosa")
-    print("eat pizza")
-    print("eat burger")
 else:
     print("do your homework")
 read = input('have read GOT?')
